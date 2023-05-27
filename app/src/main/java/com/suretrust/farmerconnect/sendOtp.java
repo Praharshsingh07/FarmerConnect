326e9bd2a092d9cca1492eaa93fb34b3ad92369b
Akshit Sharma <akshitsharma01010@gmail.com>
2023-05-27 23:24:39 +0530

    register page user interface

--- app/src/main/java/com/suretrust/farmerconnect/sendOtp.java
+++ app/src/main/java/com/suretrust/farmerconnect/sendOtp.java
@@ -1,14 +1,13 @@
 package com.suretrust.farmerconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class sendOtp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_send_otp);
     }
 }
