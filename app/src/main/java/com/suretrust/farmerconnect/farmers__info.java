0c5aa80cd34a7c20e16b164f78ede9409f4dec9c
Praharshsingh07 <Praharsh.singh@s.amity.edu>
2023-07-01 12:32:31 +0530

    commit(farmers_info)

---
+++ app/src/main/java/com/suretrust/farmerconnect/farmers__info.java
@@ -0,0 +1,19 @@
+package com.suretrust.farmerconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class farmers__info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmers_info);
+    }
+}
+
+
+
+
+
